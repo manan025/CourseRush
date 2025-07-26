4b18fd649fa3c5528392f181e08e0cceaf4ea8e7
Manan Raina Kumar <mananman23@gmail.com>
2025-07-26 19:47:46 +0530

    headless option update

--- main.py
+++ main.py
@@ -28,6 +28,11 @@ prev_available_mail = ""
 while True:
     if x != "n" and x != 'no':
         chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")  # Important for VPS
+        chrome_options.add_argument("--disable-dev-shm-usage")  # Important for VPS
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--remote-debugging-port=9222")
 
     driver = webdriver.Chrome(options=chrome_options)
 
